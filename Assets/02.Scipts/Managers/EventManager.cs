b915509c96571acac417510d5bf202de6ff4a7f2
Unity12 sungjae <sj070600@gmail.com>
2025-11-17 09:43:05 +0900

    feat: 매니저 구조 구성

---
+++ Assets/02.Scipts/Managers/EventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//게임 효과 관리 (아이템 효과, 버프/디버프 지속시간, 점수증가, 장애물 충돌. 
+
+public class EventManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/02.Scipts/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//게임 상태, 카운트 다운, 게임 오버/클리어
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("GameManager").AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
